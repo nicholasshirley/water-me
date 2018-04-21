dfb0d1555c8b8b2d63fbfa96a1d1251dfd23fe97
Nicholas Shirley <nicholas@reallymy.email>
2018-04-21 11:55:30 +0200

    create action for new log entry includes TODO
    the log entry right now flashes and directs. The flashes need
    to be better integrated into Vue (for sure the errors, but probably
    also the success messages)

--- app/controllers/log_entries_controller.rb
+++ app/controllers/log_entries_controller.rb
@@ -13,7 +13,27 @@ class LogEntriesController < ApplicationController
 
   end
 
+  def create
+    @user = current_user
+    @entry = @user.log_entries.new(log_entry_params)
+
+    if @entry.save
+      flash[:success] = 'Entry has been saved'
+      redirect_to log_entry_path(@entry)
+    else
+      # TODO this needs to be changed to target error messages on page/in component
+      flash[:danger] = @entry.errors.full_messages
+      redirect_to new_log_entry_path
+    end
+  end
+
   def edit
 
+  end
+
+  private
+
+  def log_entry_params
+    params.require(:log_entry).permit(:date, :volume)
   end
 end
