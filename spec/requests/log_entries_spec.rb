25863c2f87197afc1be1ffe9b84f4168cebb55b5
Nicholas Shirley <nicholas@reallymy.email>
2018-04-21 13:11:18 +0200

    add specs to check for ownership
    additional context is added for when the entry would otherwise be valid,
    but the user doesn't own the record

--- spec/requests/log_entries_spec.rb
+++ spec/requests/log_entries_spec.rb
@@ -48,20 +48,34 @@ RSpec.describe 'Log entries', type: :request do
   end
 
   describe 'GET the show page for a single log entry' do
-    let(:log_entry) { FactoryBot.create(:log_entry) }
-
     context 'when the user is signed in' do
       before(:each) { sign_in user }
 
-      it 'returns the page' do
-        get log_entry_path(log_entry)
+      context 'and owns the record' do
+        let(:log_entry) { FactoryBot.create(:log_entry, user_id: user.id) }
 
-        expect(response).to have_http_status(200)
+        it 'returns the page' do
+          get log_entry_path(log_entry)
+
+          expect(response).to have_http_status(200)
+        end
+      end
+
+      context 'but does not own the record' do
+        let(:log_entry) { FactoryBot.create(:log_entry) }
+
+        it 'redirects with an error message' do
+          get log_entry_path(log_entry)
+
+          expect(flash[:danger]).not_to be_empty
+          expect(response).to redirect_to log_entries_path
+        end
       end
     end
 
     context 'when the user is not sigend in' do
       before(:each) { sign_out user }
+      let(:log_entry) { FactoryBot.create(:log_entry) }
 
       it 'redirects to sign in' do
         get log_entry_path(log_entry)
@@ -72,20 +86,34 @@ RSpec.describe 'Log entries', type: :request do
   end
 
   describe 'GET an edit form for a single log entry' do
-    let(:log_entry) { FactoryBot.create(:log_entry) }
-
     context 'when the user is logged in' do
       before(:each) { sign_in user }
 
-      it 'returns the page' do
-        get edit_log_entry_path(log_entry)
+      context 'and owns the record' do
+        let(:log_entry) { FactoryBot.create(:log_entry, user_id: user.id) }
 
-        expect(response).to have_http_status(200)
+        it 'returns the page' do
+          get edit_log_entry_path(log_entry)
+
+          expect(response).to have_http_status(200)
+        end
+      end
+
+      context 'but does not own the record' do
+        let(:log_entry) { FactoryBot.create(:log_entry) }
+
+        it 'redirects with an error message' do
+          get edit_log_entry_path(log_entry)
+
+          expect(flash[:danger]).not_to be_empty
+          expect(response).to redirect_to log_entries_path
+        end
       end
     end
 
     context 'when the user is not logged in' do
       before(:each) { sign_out user }
+      let(:log_entry) { FactoryBot.create(:log_entry) }
 
       it 'redirects to sign in' do
         get edit_log_entry_path(log_entry)
@@ -171,31 +199,56 @@ RSpec.describe 'Log entries', type: :request do
   end
 
   describe 'PATCH/PUT an existing log entry' do
-    before(:each) { FactoryBot.create(:log_entry) }
     valid_patch_params = { log_entry: { date: Date.tomorrow, volume: 20 } }
 
     context 'when the user is signed in' do
       before(:each) { sign_in user }
 
       context 'and the update is well formed' do
-        it 'updates the entry' do
-          patch log_entry_path(LogEntry.last), params: valid_patch_params
+        context 'and the user owns the record' do
+          before(:each) { FactoryBot.create(:log_entry, user_id: user.id) }
 
-          expect(LogEntry.last).to have_attributes(
-            date: Date.tomorrow,
-            volume: 20
-          )
+          it 'updates the entry' do
+            patch log_entry_path(LogEntry.last), params: valid_patch_params
+
+            expect(LogEntry.last).to have_attributes(
+              date: Date.tomorrow,
+              volume: 20
+            )
+          end
+
+          it 'redirects with a success message' do
+            patch log_entry_path(LogEntry.last), params: valid_patch_params
+
+            expect(flash[:success]).not_to be_empty
+            expect(response).to redirect_to log_entry_path(LogEntry.last)
+          end
         end
 
-        it 'redirects with a success message' do
-          patch log_entry_path(LogEntry.last), params: valid_patch_params
+        context 'but the user does not own the record' do
+          before(:each) { FactoryBot.create(:log_entry) }
 
-          expect(flash[:success]).not_to be_empty
-          expect(response).to redirect_to log_entry_path(LogEntry.last)
+          it 'does not updates the entry' do
+            patch log_entry_path(LogEntry.last), params: valid_patch_params
+
+            expect(LogEntry.last).not_to have_attributes(
+              date: Date.tomorrow,
+              volume: 20
+            )
+          end
+
+          it 'redirects with an error message' do
+            patch log_entry_path(LogEntry.last), params: valid_patch_params
+
+            expect(flash[:danger]).not_to be_empty
+            expect(response).to redirect_to log_entries_path
+          end
         end
       end
 
       context 'but the update is not well formed:' do
+        before(:each) { FactoryBot.create(:log_entry, user_id: user.id) }
+
         context 'the date is removed' do
           empty_date_params = { log_entry: { date: '', volume: 20 } }
 
@@ -240,6 +293,7 @@ RSpec.describe 'Log entries', type: :request do
 
     context 'when the user is not siged in' do
       before(:each) { sign_out user }
+      before(:each) { FactoryBot.create(:log_entry) }
 
       it 'does not update the entry' do
         patch log_entry_path(LogEntry.last), params: valid_patch_params
@@ -259,26 +313,46 @@ RSpec.describe 'Log entries', type: :request do
   end
 
   describe 'DELETE an existing log entry' do
-    before(:each) { FactoryBot.create(:log_entry) }
-
     context 'when the user is logged in' do
       before(:each) { sign_in user }
 
-      it 'remove the record' do
-        expect {
+      context 'and owns the record' do
+        before(:each) { FactoryBot.create(:log_entry, user_id: user.id) }
+
+        it 'remove the record' do
+          expect {
+            delete log_entry_path(LogEntry.last)
+          }.to change{ LogEntry.count }.by(-1)
+        end
+
+        it 'redirects with a success message' do
           delete log_entry_path(LogEntry.last)
-        }.to change{ LogEntry.count }.by(-1)
+
+          expect(flash[:success]).not_to be_empty
+        end
       end
 
-      it 'redirects with a success message' do
-        delete log_entry_path(LogEntry.last)
+      context 'but does not own the record' do
+        before(:each) { FactoryBot.create(:log_entry) }
 
-        expect(flash[:success]).not_to be_empty
+        it 'does not remove the record' do
+          expect {
+            delete log_entry_path(LogEntry.last)
+          }.not_to change{ LogEntry.count }
+        end
+
+        it 'redirects with an error' do
+          delete log_entry_path(LogEntry.last)
+
+          expect(flash[:danger]).not_to be_empty
+          expect(response).to redirect_to log_entries_path
+        end
       end
     end
 
     context 'when the user is not logged in' do
       before(:each) { sign_out user }
+      before(:each) { FactoryBot.create(:log_entry) }
 
       it 'does not remove the record' do
         expect {
